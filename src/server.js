8117f3997ed68e823f5e75f72dba98c0fb8b2659
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-10-19 23:22:15 -0700

    Complete person route.

--- src/server.js
+++ src/server.js
@@ -1,14 +1,22 @@
 'use strict';
 
 const express = require('express');
+const logger = require('./middleware/logger.js');
+const validator = require('./middleware/validator');
+const error404 = require('./error-handlers/404.js');
+const error500 = require('./error-handlers/500.js');
 
 const app = express();
-
+app.use(logger);
 
 
 app.get('/person', (request, response) => {
-    response.status(200).json('I am a name.')
+    response.status(200).send({ 'name': `${request.query.name}` });
 });
+
+
+app.use(error404);
+app.use(error500);
 
 const start = (PORT) => {
     app.listen(PORT, () => {
